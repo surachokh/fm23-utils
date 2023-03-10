3b377e268b290131c9a1618c2456ce0721322791
surachok <surachok@via.link>
2023-03-10 11:57:27 +0700

    Init Project.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,8 @@
 import BodyCard from "@/components/BodyCard";
-import { Card, Typography } from "antd";
-import Head from "next/head";
 
 const LandingPage = () => {
   return (
     <>
-      <Head>
-        <title>FM 23 Utilities Program</title>
-        <link rel="icon" href="/football-ball.png" />
-      </Head>
       <BodyCard>Test</BodyCard>
     </>
   );
